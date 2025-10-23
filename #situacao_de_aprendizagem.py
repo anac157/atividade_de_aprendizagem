a59b8dc44a37dd4845b2b440f8320de86179bbb1
anac157 <danacarolina620@gmail.com>
2025-10-22 21:47:09 -0300

    atividade_python

---
+++ #situacao_de_aprendizagem.py
@@ -0,0 +1,28 @@
+#LIMITE_MÁXIMO=50
+#mylist=[]
+#contador=0
+#numero=input("digite o número da linha:")
+#print(f"o numero da linha digitado é:{numero}")
+#nome=input("digite o nome da linha:")
+#print(f"o nome da linha digitado é: {nome}")
+#passageiros = input("Digite a quantidade de passageiros na hora de pico: ")
+#passageiros=int(passageiros)
+#print(f"o numero de passageiros digitado é:{passageiros}")
+#if passageiros > LIMITE_MÁXIMO:
+#  print(">> Linha sobrecarregada — adicionar mais ônibus")
+#else:
+#  print(">> Linha com demanda normal")
+#passageiros = input("Digite a quantidade de passageiros na hora de pico: ")
+linhas_sobrecarregadas = 0
+condicao = False # Substitua 'True' pela sua condição real, como (demanda > limite)
+
+if condicao:
+    print(">> Linha sobrecarregada — adicionar mais ônibus")
+    linhas_sobrecarregadas += 1
+else:
+    print(">> Linha com demanda normal")
+
+# print(f"Total de linhas sobrecarregadas: {linhas_sobrecarregadas}")
+
+
+
